5081a37d542a0eecfddda2b93f9f663ec1bf6a3e
Ba1nky <95933124+Ba1nky@users.noreply.github.com>
2022-10-02 11:50:36 +0300

    Add files via upload

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        Phone [] arrayOfPhones = { 
+            new Phone
+            {
+                Id = 1,
+                Model = "Xiaomi Redmi Note 11 Pro",
+                OperatingSystem = "Android",
+                RAM = 6,
+                BuiltInMemory = 128,
+                NumberOfCores = 8,
+                BatteryCapacity = 5000,
+                ScreenDiagonal = 6.67F,
+                Price = 11999
+            },
+            new Phone
+            {
+                Id = 2,
+                Model = "iPhone 14",
+                OperatingSystem = "iOS",
+                RAM = 16,
+                BuiltInMemory = 128,
+                NumberOfCores = 8,
+                BatteryCapacity = 6000,
+                ScreenDiagonal = 6.1F,
+                Price = 41499
+            },
+            new Phone
+            {
+                Id = 3,
+                Model = "Samsung Galaxy S22",
+                OperatingSystem = "Android",
+                RAM = 8,
+                BuiltInMemory = 128,
+                NumberOfCores = 8,
+                BatteryCapacity = 3700,
+                ScreenDiagonal = 6.1F,
+                Price = 33199
+            },
+            new Phone
+            {
+                Id = 4,
+                Model = "Xiaomi Poco C40",
+                OperatingSystem = "Android",
+                RAM = 3,
+                BuiltInMemory = 32,
+                NumberOfCores = 8,
+                BatteryCapacity = 6000,
+                ScreenDiagonal = 6.71F,
+                Price = 6169
+            }
+            ,
+            new Phone
+            {
+                Id = 5,
+                Model = "Samsung Galaxy A12",
+                OperatingSystem = "Android",
+                RAM = 4,
+                BuiltInMemory = 64,
+                NumberOfCores = 8,
+                BatteryCapacity = 5000,
+                ScreenDiagonal = 6.71F,
+                Price = 8925
+            }
+        };
+
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(arrayOfPhones);
+        }
+
+        [HttpGet("Characteristics")]
+        public ActionResult Characteristics()
+        {
+            return View(arrayOfPhones);
+        }
+    }
+}
---
+++ WebApplication1/Models/Phone.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplication1.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Model { get; set; } = null!;
+        public string OperatingSystem { get; set; } = null!;
+        public int RAM { get; set; }
+        public int BuiltInMemory { get; set; }
+        public int NumberOfCores { get; set; }
+        public int BatteryCapacity { get; set; }//мА·год
+        public float ScreenDiagonal { get; set; }
+        public int Price { get; set; }
+    }
+}
